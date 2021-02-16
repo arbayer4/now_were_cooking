3fa1856cc649ad357f62b56c5b6394ccd42afe35
Andrew Bayer <arbayer@iu.edu>
2021-02-16 12:10:02 -0500

    Initial Commit-React App Created with dependencies. API keys hidden. Ready to get hackin'

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav></nav>
+      <h1>Getting Started</h1>
+
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
